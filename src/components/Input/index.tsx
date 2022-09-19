caf34de830cc9aa69fb286b2e06e93f909612c73
Jeam Alves <jeanmalves@gmail.com>
2022-09-19 19:20:40 -0300

    Migrate Input component

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -9,7 +9,13 @@ import { useField } from '@unform/core';
 
 import { Container } from './styles';
 
-const Input = ({ name, icon: Icon, ...rest }) => {
+interface InputProps {
+  name: string;
+  placeholder: string;
+  icon?: any;
+}
+
+function Input({ name, placeholder, icon: Icon, ...rest }: InputProps) {
   const inputRef = useRef(null);
 
   const [isFocused, setIsFocused] = useState(false);
@@ -24,7 +30,7 @@ const Input = ({ name, icon: Icon, ...rest }) => {
   const handleInputBlur = useCallback(() => {
     setIsFocused(false);
 
-    setIsFilled(!!inputRef.current?.value);
+    setIsFilled(!!inputRef.current);
   }, []);
 
   useEffect(() => {
@@ -44,10 +50,10 @@ const Input = ({ name, icon: Icon, ...rest }) => {
         onBlur={handleInputBlur}
         defaultValue={defaultValue}
         ref={inputRef}
-        {...rest}
-      />
+        placeholder={placeholder}
+        {...rest} />
     </Container>
   );
-};
+}
 
 export default Input;
